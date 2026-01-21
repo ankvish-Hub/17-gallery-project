7f56dadc700550c0fcbf4db7d64d80acc8348d66
Ankit Vishwakarma <ankvish1102@gmail.com>
2026-01-21 11:59:43 +0530

    Gallery Project Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,68 @@
-import axios from "axios";
-import { useState, useEffect } from "react";
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Card from './components/Card';
 
 const App = () => {
+
   const [userData, setUserData] = useState([]);
-  const [index, setIndex] = useState(1);
+
+  const [index, setIndex] = useState(1)
 
   const getData = async () => {
-    const response = await axios.get(
-      `https://picsum.photos/v2/list?page=${index}&limit=10`,
-    );
-    setUserData(response.data);
-  };
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=10`)
+    setUserData(response.data)
+  }
 
   useEffect(function () {
-    getData();
-  }, [index]);
+    getData()
+  }, [index])
 
-  let printUserData = "No User Available";
+
+  let printUserData = <h3 className='text-gray-300 text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold'>Loading...</h3>
 
   if (userData.length > 0) {
-    printUserData = userData.map((user) => (
-      <div
-        key={user.id}
-        className="w-60 bg-gray-800 rounded overflow-hidden shadow-lg"
-      >
-        <img
-          src={user.download_url}
-          alt={user.author}
-          className="w-full h-40 object-cover"
-        />
-        <div className="px-4 py-2">
-          <div className="font-bold text-white text-lg mb-2">{user.author}</div>
-          <p className="text-gray-300 text-sm">ID: {user.id}</p>
-        </div>
+    printUserData = userData.map(function (elem, idx) {
+
+      return <div key={idx}>
+        <Card elem={elem} />
       </div>
-    ));
+    })
   }
 
   return (
-    <div className="h-screen bg-black text-white p-4 overflow-auto">
-      <h1 className="">Gallery Page {index}</h1>
-      <div className="flex flex-wrap gap-4">{printUserData}</div>
+    <div className='bg-black overflow-auto h-screen p-4 text-white'>
+      <div className='flex h-[82%] flex-wrap gap-4 p-2'>
+        {printUserData}
+      </div>
 
-      <div className="gap-6 flex justify-center items-center p-4">
+      <div className='flex justify-center gap-6 items-center p-4'>
         <button
-          className="bg-amber-500 text-black rounded px-4 cursor-pointer active:scale-95 font-semibold"
+          style={{ opacity: index == 1 ? 0.6 : 1 }}
+          className='bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold'
           onClick={() => {
-
-            if(index>1){
-              setIndex(index-1);
+            if (index > 1) {
+              setIndex(index - 1)
+              setUserData([])
             }
           }}
         >
           Prev
         </button>
+        <h4>Page {index}</h4>
         <button
-          className="bg-amber-500 text-black rounded px-4 cursor-pointer active:scale-95 font-semibold"
+          className='bg-amber-400 text-sm cursor-pointer active:scale-95 text-black rounded px-4 py-2 font-semibold'
           onClick={() => {
-            setIndex(index+1);
-            
+            setUserData([])
+
+            setIndex(index + 1)
+
           }}
         >
           Next
         </button>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
